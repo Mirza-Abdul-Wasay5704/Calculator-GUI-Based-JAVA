2678c0b39561fab74a5cf000dd1c46b666b8685a
Mirza-Abdul-Wasay5704 <m.a.wasayuddin143@gmail.com>
2025-01-29 13:05:19 +0500

    First Commit

---
+++ my1stgui/My1stGUI.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package my1stgui;
+
+/**
+ *
+ * @author mirza abdul wasay
+ */
+public class My1stGUI {
+
+    public float ans;
+    public float a=0;
+    public float b=0;
+   
+    public void add(){
+        ans=(a+b);
+    }
+    
+    
+    public void subtract(){
+        ans=(a-b);
+    }
+    
+    
+    public void multiply(){
+        ans=(a*b);
+    }
+    
+    
+    public void divide(){
+        ans=(a/b);
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+//    public static void main(String[] args) {
+//        // TODO code application logic here
+//    }
+    
+}
